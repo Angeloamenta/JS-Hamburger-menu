5f85c73465bb5f391e709ad47efeddd69eb13a8a
Angeloamenta <angeloamenta7@gmail.com>
2021-11-22 17:02:36 +0100

    bonus-colore

---
+++ bonus/main.js
@@ -0,0 +1,28 @@
+/*Hamburger menu:
+mostrare / nascondere il menu principale
+(per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile)
+*/
+let barsMenu = document.querySelector (".fa-bars");
+console.log (barsMenu.className);
+
+let hamburgerMenu = document.querySelector (".hamburger-menu")
+console.log(hamburgerMenu.className);
+
+barsMenu.addEventListener("click", function(){ 
+    
+    hamburgerMenu.className = "active hamburger-menu";
+    console.log(hamburgerMenu);
+}); 
+
+let closeMenu = document.querySelector (".fa-times");
+console.log (closeMenu.className);
+
+closeMenu.addEventListener("click", function(){ 
+    hamburgerMenu.className = "hamburger-menu";
+    console.log(hamburgerMenu);
+}); 
+
+//cambio colore ad un elemento
+
+let cta = document.querySelector (".cta");
+cta.style.backgroundColor = "blue";
