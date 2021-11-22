cfa792bceb8c32e9928aee272d7672b764d3966d
Angeloamenta <angeloamenta7@gmail.com>
2021-11-22 15:34:26 +0100

    selezione funzionante

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 mostrare / nascondere il menu principale
 (per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile)
 */
-let bars = document.getElementsByClassName ("fa-bars");
-console.log (bars);
-let hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+let barsMenu = document.querySelector (".fa-bars");
+console.log (barsMenu);
+let hamburgerMenu = document.querySelector (".hamburger-menu")
 console.log(hamburgerMenu);
 
-bars.addEventListener("click" function () {
-    let hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+barsMenu.addEventListener("click", function(){ 
     hamburgerMenu.style.display = "block"; 
-});
+}); 
