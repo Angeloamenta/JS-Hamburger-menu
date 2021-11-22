3d5cac853602f3497bc2e2973c04c8b6c764dc0b
Angeloamenta <angeloamenta7@gmail.com>
2021-11-22 15:54:39 +0100

    apertura e chiusura funzionanti

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ mostrare / nascondere il menu principale
 (per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile)
 */
 let barsMenu = document.querySelector (".fa-bars");
-console.log (barsMenu);
+console.log (barsMenu.className);
+
 let hamburgerMenu = document.querySelector (".hamburger-menu")
-console.log(hamburgerMenu);
+console.log(hamburgerMenu.className);
 
 barsMenu.addEventListener("click", function(){ 
     hamburgerMenu.style.display = "block"; 
+}); 
+
+let closeMenu = document.querySelector (".fa-times");
+console.log (closeMenu.className);
+
+closeMenu.addEventListener("click", function(){ 
+    hamburgerMenu.style.display = "none"; 
 }); 
